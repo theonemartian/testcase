e0f2bb3d8216f7360d36e9ef319c0ba6d0397f52
theonemartian <48899770+theonemartian@users.noreply.github.com>
2023-03-26 21:55:38 +0530

    Update settings.py

--- testcase/testcase/settings.py
+++ testcase/testcase/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8slr0ee=7eh*s59$5ar!g@pp24-n^#y^y1f$%bwq@fhfc&j=+0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.25.59.254']
+ALLOWED_HOSTS = ['3.25.59.254','127.0.0.1']
 
 
 # Application definition
